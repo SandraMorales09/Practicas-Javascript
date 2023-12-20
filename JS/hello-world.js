5f30c9b8de72ff2ab0f0a38387a628c5651ef70b
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2023-12-20 00:40:48 -0600

    Add files via upload

---
+++ JS/hello-world.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise, It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// string in 25 "" or ''
+
+export function hello() {
+    return 'Hello, World';
+}
